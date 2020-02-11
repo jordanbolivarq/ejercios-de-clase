641c2bf710e7211183da237c51589d5bf141688a
jordanbolivarq <60189623+jordanbolivarq@users.noreply.github.com>
2020-02-11 09:15:41 -0500

    Create While.cs

---
+++ While.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double total = 0;
+            Console.WriteLine("ingrese numero de datos: ");
+            int n = int.Parse(Console.ReadLine());
+            int contador = 0;
+            int max = 0;
+            int min = 0;
+            string nombreMax = "";
+            string nombreMin = "";
+
+
+            while (contador < n)
+            {
+                Console.WriteLine("edad: ");
+                int edad = int.Parse(Console.ReadLine());
+                Console.WriteLine("nombre: ");
+                string nombre = (Console.ReadLine());
+                if (contador == 0)
+                {
+                    max = edad;
+                    min = edad;
+                }
+                if (edad < min)
+                {
+                    max = edad;
+                    nombreMax = nombre;
+                }
+
+                if (edad > max)
+                {
+                    min = edad;
+                    nombreMin = nombre;
+                }
+
+                total += edad;
+                contador++;
+            }
+
+            double promedio = total / n;
+            Console.WriteLine("promedio: " + promedio);
+            Console.WriteLine("maximo: " + max + nombreMax);
+            Console.WriteLine("minimo: " + min + nombreMin);
